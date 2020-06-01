29392da4da11462da641a6a08511a160809ba4d1
rahul975337 <55583515+rahul975337@users.noreply.github.com>
2020-06-01 21:43:24 +0530

    updated icons

--- app/src/main/java/com/rt/apps/rrassociates/MainActivity.java
+++ app/src/main/java/com/rt/apps/rrassociates/MainActivity.java
@@ -6,74 +6,23 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Menu;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.core.view.GravityCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    private AppBarConfiguration mAppBarConfiguration;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-//        NavigationView navigationView = findViewById(R.id.nav_view);
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        mAppBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-//                .setDrawerLayout(drawer)
-//                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-//        NavigationUI.setupWithNavController(navigationView, navController);
-//    }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onSupportNavigateUp() {
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-//                || super.onSupportNavigateUp();
-//    }
-//}
-////////////////////////////////
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 private WebView wv;
 @Override
@@ -110,7 +59,7 @@ public void onClick(View view) {
         settings.setUseWideViewPort(true);
         settings.setSavePassword(true);
         settings.setSaveFormData(true);
-        wv.loadUrl("https://www.rrassociateswealth.com/");
+        wv.loadUrl("https://tilwani03.wixsite.com/website");
         wv.setWebViewClient(new MywebViewClient());
         }
 @Override
@@ -135,9 +84,9 @@ public boolean onOptionsItemSelected(MenuItem item) {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-        return true;
-        }
+//        if (id == R.id.action_settings) {
+//        return true;
+//        }
         return super.onOptionsItemSelected(item);
         }
 @SuppressWarnings("StatementWithEmptyBody")
@@ -153,9 +102,7 @@ public boolean onNavigationItemSelected(MenuItem item) {
         }else if (id == R.id.nav_contact) {
         wv.loadUrl("https://www.rrassociateswealth.com/contact");
         }
-         else if(id == R.id.chat){
-             wv.loadUrl("https://www.rrassociateswealth.com/contact");
-         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
---
+++ app/src/main/java/com/rt/apps/rrassociates/SplashActivity.java
@@ -0,0 +1,78 @@
+package com.rt.apps.rrassociates;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+import android.view.MotionEvent;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.ref.WeakReference;
+public class SplashActivity extends Activity {
+
+
+    //Private declarations
+    private InternalHandler mHandler;
+    private long mStartTime;
+
+
+    //Constants
+    private static final int GO_AHEAD = 1;
+    private static final long MAX_TIME = 1500L;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mStartTime = SystemClock.uptimeMillis();
+        mHandler = new InternalHandler(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        final Message goAHeadMessage = mHandler.obtainMessage(GO_AHEAD);
+        mHandler.sendMessageAtTime(goAHeadMessage, mStartTime + MAX_TIME);
+
+    }
+
+    private void endSplash() {
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
+        finish();
+
+    }
+
+
+    private static class InternalHandler extends Handler {
+
+        private WeakReference<SplashActivity> mSAWeakRef;
+
+        public InternalHandler(SplashActivity mSARef) {
+            this.mSAWeakRef = new WeakReference<SplashActivity>(mSARef);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            final SplashActivity mActivity = mSAWeakRef.get();
+            if (mActivity == null)
+                return;
+            switch (msg.what) {
+                case GO_AHEAD:
+                    long elapsedTime = SystemClock.uptimeMillis() - mActivity.mStartTime;
+                    if (elapsedTime >= MAX_TIME) {
+                        mActivity.endSplash();
+                    }
+                    break;
+            }
+        }
+    }
+}
